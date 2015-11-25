8b813166318ebc92f2a99fff64a9bb0164c41243
Cerie <bad.cerie@yandex.com>
2015-11-25 10:27:06 -0600

    QE Drawing patch
    Add circle draws for Q and E spells

---
+++ EloBuddy-Addons/AddonTemplate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+
+namespace AddonTemplate
+{
+    public static class Program
+    {
+        // Set addon ChampName to Soraka
+        // Enforces AddOn to work for her alone
+
+        public const string ChampName = "Soraka";
+
+        public static void Main(string[] args)
+        {
+            // Wait till the loading screen has passed
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            // Verify the champion we made this addon for
+            if (Player.Instance.ChampionName != ChampName)
+            {
+                // Champion is not the one we made this addon for,
+                // therefore we return
+                return;
+            }
+
+            // Initialize the classes that we need
+            Config.Initialize();
+            SpellManager.Initialize();
+            ModeManager.Initialize();
+
+            // Listen to events we need
+            Drawing.OnDraw += OnDraw;
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+            // Draw Q range in Gold
+            Circle.Draw(Color.Gold, SpellManager.Q.Range, Player.Instance.Position);
+
+            //Circle.Draw(Color.Red, SpellManager.W.Range, Player.Instance.Position);
+            
+            // Draw E range in Blue
+            Circle.Draw(Color.Blue, SpellManager.E.Range, Player.Instance.Position);
+            
+            //Circle.Draw(Color.Red, SpellManager.R.Range, Player.Instance.Position);
+        }
+    }
+}
---
+++ EloBuddy-Addons/AddonTemplate/SpellManager.cs
@@ -0,0 +1,44 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+
+namespace AddonTemplate
+{
+    public static class SpellManager
+    {   /*
+         * 
+         * Soraka Spellbook
+         * 
+         * Q - Starcall (Skillshot)
+         * 
+         * W - Astral Infustion (Targeted)
+         * 
+         * E - Equinox (Skillshot)
+         * 
+         * R - Wish (Active)
+         * 
+         */
+        public static Spell.Skillshot Q { get; private set; }
+        public static Spell.Targeted W { get; private set; }
+        public static Spell.Skillshot E { get; private set; }
+        public static Spell.Active R { get; private set; }
+
+        static SpellManager()
+        {
+            /*
+             * Sell data ripped from 
+             * https://github.com/KarmaPanda/EloBuddy/blob/master/SorakaBuddy/Program.cs
+             * 
+             */
+            Q = new Spell.Skillshot(SpellSlot.Q, 950, SkillShotType.Circular, 283, 1100, 210);
+            W = new Spell.Targeted(SpellSlot.W, 550);
+            E = new Spell.Skillshot(SpellSlot.E, 925, SkillShotType.Circular, 500, 1750, 70);
+            R = new Spell.Active(SpellSlot.R, int.MaxValue);
+        }
+
+        public static void Initialize()
+        {
+            // Let the static initializer do the job, this way we avoid multiple init calls aswell
+        }
+    }
+}
